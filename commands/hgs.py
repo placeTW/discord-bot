a4f06b22d5cd9980e0c5af3357657741144e2215
chanomkaimuk <22185824+chanomkaimuk@users.noreply.github.com>
2023-07-31 01:58:40 +0200

    HGS button pings people now

--- commands/hgs.py
+++ commands/hgs.py
@@ -1,22 +1,24 @@
 import discord
 
 
-class ButtonView(discord.ui.View):
+class HGSButtonView(discord.ui.View):
     def __init__(self):
         super().__init__()
 
-    @discord.ui.button(label="Yes", style=discord.ButtonStyle.gray)
+    @discord.ui.button(label="Yes", style=discord.ButtonStyle.green)
     async def hgs_button_yes(
         self, interaction: discord.Interaction, button: discord.ui.Button
     ):
-        await interaction.response.send_message("Correct")
+        user_id = interaction.user.id
+        await interaction.response.send_message(f"<@{user_id}> is correct")
 
-    @discord.ui.button(label="No", style=discord.ButtonStyle.gray)
+    @discord.ui.button(label="No", style=discord.ButtonStyle.blurple)
     async def hgs_button_no(
         self, interaction: discord.Interaction, button: discord.ui.Button
     ):
+        user_id = interaction.user.id
         await interaction.response.send_message(
-            "Your opinion has been ignored"
+            f"<@{user_id}>'s opinion has been ignored"
         )
 
 
@@ -27,5 +29,5 @@ def register_commands(tree, this_guild: discord.Object):
         guild=this_guild,
     )
     async def hgs(interaction: discord.Interaction):
-        button = ButtonView()
+        button = HGSButtonView()
         await interaction.response.send_message("Hot gay sex?", view=button)
