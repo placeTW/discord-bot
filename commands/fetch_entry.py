db059aa01b757d5a6cb574fb82e40719e616808a
chanomkaimuk <22185824+chanomkaimuk@users.noreply.github.com>
2023-07-30 18:28:26 +0200

    fetch_entry supports langauge and field (with restrictions_

--- commands/fetch_entry.py
+++ commands/fetch_entry.py
@@ -2,21 +2,46 @@ import aiohttp
 import discord
 from discord import app_commands
 from .modules import async_utils
+import typing
+
+SUPPORTED_LANGUAGE_CODES = typing.Literal["en", "et", "lt", "lv", "zh", "fr"]
+SUPPORTED_ART_FIELDS = typing.Literal["title", "blurb", "desc"]
+
+
+async def get_json(how="url", json_url=""):
+    assert how in ("url",)
+    if how == "url":
+        result = await async_utils._async_get_json(json_url)
+    return result
 
 
 def register_commands(tree, this_guild: discord.Object):
     @tree.command(
         name="fetch",
-        description="Fetches something idk",
+        description="Fetches r/place Taiwan art entries",
         guild=this_guild,
     )
-    @app_commands.describe(index="a number between 0 and 15")
-    async def fetch_entry(interaction: discord.Interaction, index: int):
-        result_json = await async_utils._async_get_json(
-            "https://placetw.com/locales/en/art-pieces.json"
+    async def fetch_entry(
+        interaction: discord.Interaction,
+        index: int,
+        lang: SUPPORTED_LANGUAGE_CODES,
+        field: SUPPORTED_ART_FIELDS,
+    ):
+        """This function does some shit
+
+        Args:
+            interaction (discord.Interaction): idk
+            index (int): A number. DELETE THIS
+            lang (str): The language of the entry to fetch.
+            field (str): The field to fetch: title, blurb or description.
+        """
+        link_to_fetch = f"https://placetw.com/locales/{lang}/art-pieces.json"
+        result_json = await get_json(
+            how="url",
+            json_url=link_to_fetch,
         )
         await interaction.response.send_message(
-            f"Here: `{result_json[index]['title']}`"
+            f"The {field} for {lang} is:\n`{result_json[index][field]}`"
         )
 
 
