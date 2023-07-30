7f2f4eb03689bf17205895251c7edc1826808511
chanomkaimuk <22185824+chanomkaimuk@users.noreply.github.com>
2023-07-31 01:20:59 +0200

    also error handling for no json

--- commands/modules/async_utils.py
+++ commands/modules/async_utils.py
@@ -4,7 +4,8 @@ import aiohttp
 async def _async_get_json(url: str) -> str:
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
-            # print("Status:", response.status)
-            # print("Content-type:", response.headers['content-type'])
-            json_response = await response.json()
-    return json_response
+            print("Status:", response.status)
+            print("Content-type:", response.headers["content-type"])
+            if response.headers["content-type"] != "application/json":
+                return None
+            return await response.json()
