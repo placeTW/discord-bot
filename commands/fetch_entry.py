2ffe151c2e4c534ec8f96ed988730069f925aad9
chanomkaimuk <22185824+chanomkaimuk@users.noreply.github.com>
2023-07-30 03:59:59 +0200

    get json from url

--- commands/fetch_entry.py
+++ commands/fetch_entry.py
@@ -1,1 +1,10 @@
 import requests
+
+
+def _get_json_from_url(url: str):
+    with requests.get(url) as r:
+        return r.json()[-1]
+
+
+if __name__ == "__main__":
+    print(_get_json_from_url("https://placetw.com/locales/en/art-pieces.json"))
