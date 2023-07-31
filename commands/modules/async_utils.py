20cc64bf1eef4f547846f3ed1704448586a1998d
chanomkaimuk <22185824+chanomkaimuk@users.noreply.github.com>
2023-07-31 14:56:20 +0200

    turned off print oops

--- commands/modules/async_utils.py
+++ commands/modules/async_utils.py
@@ -4,8 +4,8 @@ import aiohttp
 async def _async_get_json(url: str) -> str:
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
-            print("Status:", response.status)
-            print("Content-type:", response.headers["content-type"])
+            # print("Status:", response.status)
+            # print("Content-type:", response.headers["content-type"])
             if response.headers["content-type"] != "application/json":
                 return None
             return await response.json()
