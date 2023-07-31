e793ad2da0ccb80ed61eb25f0b6d837afcbdd2a8
chanomkaimuk <22185824+chanomkaimuk@users.noreply.github.com>
2023-07-31 21:50:14 +0200

    can send to specific channel now

--- commands/edit_entry/edit_entry_modal.py
+++ commands/edit_entry/edit_entry_modal.py
@@ -3,6 +3,8 @@ import discord
 from discord import ui
 from discord.utils import MISSING
 
+WAITING_APPROVAL_CHANNEL_ID = 1135250604751601716
+
 
 class Questionnaire(ui.Modal):
     name = ui.TextInput(label="Name")
@@ -10,24 +12,33 @@ class Questionnaire(ui.Modal):
 
     def __init__(
         self,
+        client: discord.Client,
         *,
         title: str,
         timeout: float = None,
     ) -> None:
         super().__init__(title=title, timeout=timeout)
+        self.the_client = client
+        self.approval_channel = client.get_channel(WAITING_APPROVAL_CHANNEL_ID)
 
     async def on_submit(self, interaction: discord.Interaction):
         await interaction.response.send_message(
             f"Thanks for your response, {self.name}!", ephemeral=True
         )
+        user_name = interaction.user.name
+        await self.approval_channel.send(
+            f"user {user_name} sent `{self.answer}`"
+        )
 
 
-def register_commands(tree, this_guild: discord.Object):
+def register_commands(
+    tree, this_guild: discord.Object, client: discord.Client
+):
     @tree.command(
         name="edit-entry",
         description="Edits an entry via form",
         guild=this_guild,
     )
     async def call_form(interaction: discord.Interaction):
-        form = Questionnaire(title="Questionnaire")
+        form = Questionnaire(client, title="Questionnaire")
         await interaction.response.send_modal(form)
