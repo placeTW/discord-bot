1c94f8a34a98bc07f5da05faf14766d4f6e03fac
chanomkaimuk <22185824+chanomkaimuk@users.noreply.github.com>
2023-08-04 17:41:20 +0200

    properly test get_json en

--- tests/test_get_json.py
+++ tests/test_get_json.py
@@ -7,3 +7,15 @@ async def test_get_json_en():
         how="url",
         json_url=link_to_fetch,
     )
+    assert type(result_json) == dict
+    assert "capoo" in result_json
+    assert "asdf" not in result_json
+    for art_id, art_info in result_json.items():
+        assert "title" in art_info
+        assert type(art_info["title"]) == str
+        assert "blurb" in art_info
+        assert type(art_info["blurb"]) == str
+        assert "desc" in art_info
+        assert type(art_info["desc"]) == str
+        assert "links" in art_info
+        assert type(art_info["links"]) == list
