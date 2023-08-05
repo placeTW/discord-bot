612d47352421ccae0bff35563028741d5e29e1f3
chanomkaimuk <22185824+chanomkaimuk@users.noreply.github.com>
2023-08-06 01:39:22 +0200

    more postprocess tests

--- tests/test_postprocess.py
+++ tests/test_postprocess.py
@@ -3,6 +3,7 @@ import commands.modules.postprocess as postprocess
 
 def test_postprocess_fetch_field():
     assert postprocess.postprocess_fetch_field("hi") == "hi"
+    assert postprocess.postprocess_fetch_field("") == ""
     assert (
         postprocess.postprocess_fetch_field(["a", "b", "c"]) == "* a\n* b\n* c"
     )
