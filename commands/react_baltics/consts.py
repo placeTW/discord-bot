bcc7b983a3fff4e5ad8127d5255aff91b0de0e3d
chanomkaimuk <22185824+chanomkaimuk@users.noreply.github.com>
2023-08-14 01:48:31 +0200

    LT and LV hgs react in TW + tests

--- commands/react_baltics/consts.py
+++ commands/react_baltics/consts.py
@@ -7,9 +7,13 @@ POSSIBLE_REACTS = (
 )
 
 LT_REGEX_STR = r"Taivan(?:as|a|e|o|ui|ie(?:tiškas|tis|tė|či(?:ų|u|ai|ui|ams)))"
+LT_REGEX_STR_TW = "立陶宛"
+
 LV_REGEX_STR = (
     r"Taivān(?:a|ā|ai|as|u|isks|ie(?:tis|te|šu|tim|tei|šiem|ti|te|ši|tes|ši))"
 )
+LV_REGEX_STR_TW = "拉脫維亞"
+
 
 KEYWORDS = (
     # Lithuanian
@@ -19,9 +23,14 @@ KEYWORDS = (
     # ! Estonian (STILL MISSING)
 )
 
-LT_REGEX = compile(rf"\b(?:{LT_REGEX_STR})\b", flags=IGNORECASE | UNICODE)
-LV_REGEX = compile(rf"\b(?:{LV_REGEX_STR})\b", flags=IGNORECASE | UNICODE)
+LT_REGEX = compile(
+    rf"\b(?:{LT_REGEX_STR})\b|{LT_REGEX_STR_TW}", flags=IGNORECASE | UNICODE
+)
+LV_REGEX = compile(
+    rf"\b(?:{LV_REGEX_STR})\b|{LV_REGEX_STR_TW}", flags=IGNORECASE | UNICODE
+)
 
 BALTIC_REGEX = compile(
-    rf"\b(?:{'|'.join(KEYWORDS)})\b", flags=IGNORECASE | UNICODE
+    rf"\b(?:{'|'.join(KEYWORDS)})\b|{LV_REGEX_STR_TW}|{LT_REGEX_STR_TW}",
+    flags=IGNORECASE | UNICODE,
 )
