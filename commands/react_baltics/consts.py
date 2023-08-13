d5cddd6b13b83e4db712289d3525854aa0c1c89c
chanomkaimuk <22185824+chanomkaimuk@users.noreply.github.com>
2023-08-14 01:37:34 +0200

    LT and LV hgs reacts

--- commands/react_baltics/consts.py
+++ commands/react_baltics/consts.py
@@ -3,16 +3,24 @@ from re import compile, IGNORECASE, UNICODE
 POSSIBLE_REACTS = (
     "<:tw_baltics_heart:1132524940587962388>",
     "<:bubblemilktea:1132632348651966596>",
+    "<:chaos:1140296704516706315>",
+)
+
+LT_REGEX_STR = r"Taivan(?:as|a|e|o|ui|ie(?:tiškas|tis|tė|či(?:ų|u|ai|ui|ams)))"
+LV_REGEX_STR = (
+    r"Taivān(?:a|ā|ai|as|u|isks|ie(?:tis|te|šu|tim|tei|šiem|ti|te|ši|tes|ši))"
 )
 
 KEYWORDS = (
     # Lithuanian
-    r"Taivan(?:as|a|e|o|ui|ie(?:tiškas|tis|tė|či(?:ų|u|ai|ui|ams)))",
+    LT_REGEX_STR,
     # Latvian
-    r"Taivān(?:a|ā|ai|as|u|isks|ie(?:tis|te|šu|tim|tei|šiem|ti|te|ši|tes|ši))",
+    LV_REGEX_STR,
     # ! Estonian (STILL MISSING)
 )
 
+LT_REGEX = compile(rf"\b(?:{LT_REGEX_STR})\b", flags=IGNORECASE | UNICODE)
+LV_REGEX = compile(rf"\b(?:{LV_REGEX_STR})\b", flags=IGNORECASE | UNICODE)
 
 BALTIC_REGEX = compile(
     rf"\b(?:{'|'.join(KEYWORDS)})\b", flags=IGNORECASE | UNICODE
