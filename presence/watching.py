63550a53c2b260eccf6b08022664848825daa017
chanomkaimuk <22185824+chanomkaimuk@users.noreply.github.com>
2023-08-15 17:27:22 +0200

    even more movies/series

--- presence/watching.py
+++ presence/watching.py
@@ -2,27 +2,7 @@ from random import choice
 import discord
 from discord.app_commands import Choice
 from discord import app_commands
-
-LIST_OF_MOVIES = (
-    "City of Sadness / 悲情城市",
-    "7 Days in Heaven / 父後七日",
-    "Detention / 返校",
-    "Cape No. 7 / 海角七號",
-    "The World Between Us / 我們與惡的距離",
-    "Spicy Teacher / 麻辣鮮師",
-    "Warriors Of The Rainbow / 賽德克·巴萊",
-    "The Teenage Psychic / 通靈少女",
-    "KANO (2014)",
-    "Incantation / 咒",
-    "You Are the Apple of My Eye / 那些年，我們一起追的女孩",
-    "Din Tao: Leader of the Parade / 陣頭",
-    "Light the Night / 華燈初上",
-    "Gold Leaf / 茶金",
-)
-
-MOVIE_CHOICES = [
-    Choice(name=movie_name, value=movie_name) for movie_name in LIST_OF_MOVIES
-]
+from .watching_consts import LIST_OF_MOVIES, MOVIE_CHOICES
 
 
 def get_random_movie():
