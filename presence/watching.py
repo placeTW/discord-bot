2c2ccc8e2accdbf022a0f2cb92ed395b2c865115
chanomkaimuk <22185824+chanomkaimuk@users.noreply.github.com>
2023-08-15 14:10:26 +0200

    more movies/series

--- presence/watching.py
+++ presence/watching.py
@@ -8,6 +8,16 @@ LIST_OF_MOVIES = (
     "7 Days in Heaven / 父後七日",
     "Detention / 返校",
     "Cape No. 7 / 海角七號",
+    "The World Between Us / 我們與惡的距離",
+    "Spicy Teacher / 麻辣鮮師",
+    "Warriors Of The Rainbow / 賽德克·巴萊",
+    "The Teenage Psychic / 通靈少女",
+    "KANO (2014)",
+    "Incantation / 咒",
+    "You Are the Apple of My Eye / 那些年，我們一起追的女孩",
+    "Din Tao: Leader of the Parade / 陣頭",
+    "Light the Night / 華燈初上",
+    "Gold Leaf / 茶金",
 )
 
 MOVIE_CHOICES = [
