682871505ddc44ddf46cda9056a64445d60badbc
Howard Tseng <howardt12345@gmail.com>
2023-10-12 21:31:01 -0400

    optimization

--- commands/modules/logging.py
+++ commands/modules/logging.py
@@ -5,9 +5,8 @@ filename = f"{str(datetime.datetime.now()).split('.')[0].replace(':', '-')}.log"
 path = f"{sys.path[0]}/logs/{filename}"
 
 
-file_object = open(path, 'a')
-
-
 def log(message):
     print(message)
+    file_object = open(path, 'a')
     file_object.write(f"{datetime.datetime.now()}: {message}\n")
+    file_object.close()
