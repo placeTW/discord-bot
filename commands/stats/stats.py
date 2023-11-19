8672380706ee33f3f0d54cd9fef988ecf4b1ea5a
Howard Tseng <howardt12345@gmail.com>
2023-11-19 00:52:12 -0500

    refractor

--- commands/stats/stats.py
+++ commands/stats/stats.py
@@ -2,7 +2,7 @@ import discord
 from discord import app_commands
 
 from bot import TWPlaceClient
-from commands.stats.stats_functions import get_pat_stats
+from commands.stats.pat_stats import pat_stats_commands
 from ..modules.supabase import supabaseClient
 
 
@@ -12,33 +12,7 @@ def register_commands(
     guilds: list[discord.Object],
 ):
     stats = app_commands.Group(name="stats", description="Stats functions")
-
-    pat_stats = app_commands.Group(name="pat",description="Pat stats")
-
-    @pat_stats.command(name='patted', description="Total patted stats")
-    async def patted_stats(interaction: discord.Interaction):
-        embed = get_pat_stats('patted')
-        if embed is None:
-            await interaction.response.send_message(
-                f"There was an error getting the patted stats. Please try again",
-                ephemeral=True,
-            )
-            return
-
-        await interaction.response.send_message(embed=embed)
-        
-
-    @pat_stats.command(name='patter', description="Total patter stats")
-    async def patted_stats(interaction: discord.Interaction):
-        embed = get_pat_stats('patter')
-        if embed is None:
-            await interaction.response.send_message(
-                f"There was an error getting the patter stats. Please try again",
-                ephemeral=True,
-            )
-            return
-
-        await interaction.response.send_message(embed=embed)
+    pat_stats = pat_stats_commands()
 
     stats.add_command(pat_stats)
     tree.add_command(stats, guilds=guilds)
