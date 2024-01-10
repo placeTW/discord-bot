66d56f11b0f678bab97869e7094ff486343e48f7
Howard Tseng <howardt12345@gmail.com>
2024-01-10 13:37:43 -0500

    refractor: moved send_sticker to functions

--- commands/formosa_stickers/functions.py
+++ commands/formosa_stickers/functions.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 import urllib3
 from io import BytesIO
+import random
+import discord
+
+from ..modules import logging
 
 from .consts import POSSIBLE_URLS, SITE_URL
 
@@ -39,3 +43,40 @@ def fetch_sticker(sticker_url):
     response = http.request('GET', f'{SITE_URL}/{sticker_url}')
     data = response.data
     return BytesIO(data)
+
+
+async def send_sticker(interaction: discord.Interaction, user: discord.User = None, 
+                        type: str = 'search', style: str | None = None, query: str | None = None, 
+                        latest: bool = False):
+    await interaction.response.defer()
+
+    # get the sticker urls
+    stickers_list = fetch_stickers_list(type, style, query)
+    if (len(stickers_list) == 0):
+        await interaction.followup.send(f"No stickers found for query: {query}", ephemeral=True)
+        return
+
+    if (latest):
+        sticker = stickers_list[0]
+    else:
+        sticker = random.choice(stickers_list)
+
+    sticker_name = sticker['url'].split('/')[-1].split('.')[0]
+    sticker_url = sticker['url']
+    # fetch the image and get as discord file
+    file = discord.File(fetch_sticker(sticker_url), filename=f'{sticker_name}.{"gif" if sticker["type"] == "gif" else "jpg"}')
+    await interaction.followup.send(f"<@{user.id}>" if user else None, file=file, ephemeral=True)
+
+    log_event = {
+        "event": "sticker",
+        "author_id": interaction.user.id,
+        "mentioned_id": user.id if user else None,
+        "metadata": {
+            'type': type,
+            'style': style,
+            'query': query,
+            "latest": latest,
+        }
+    }
+
+    await logging.log_event(interaction, log_event, content=sticker, log_to_channel=False)
