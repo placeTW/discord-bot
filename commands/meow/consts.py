cbf4db4a557cf59198e3f453ccf80f2d56076d05
chanomkaimuk <22185824+chanomkaimuk@users.noreply.github.com>
2024-07-07 04:22:43 +0200

    add JP, Baltic meows; add \b to force whole word match; fixed original regex

--- commands/meow/consts.py
+++ commands/meow/consts.py
@@ -7,19 +7,41 @@ POSSIBLE_MEOW_REACTS = [
     '<a:catArrive:1161441364869918881>',
     '<:Capoo:1139357657698938991>'
 ]
+
+JP_MEOWs = [
+    "ニャー",
+    "にゃー",
+]
+
+BALTIC_MEOWs = [
+    # Estonian
+    "mjäu",
+    # Latvian
+    "mjau",
+    # Lithuanian
+    "miau",
+]
+BALTIC_MEOWs = [rf"\b{meow}\b" for meow in BALTIC_MEOWs] # to match whole words only
+
+CZECH_MEOWs = [
+    "mňau",
+]
+CZECH_MEOWs = [rf"\b{meow}\b" for meow in CZECH_MEOWs] # to match whole words only
+
 POSSIBLE_MEOW_MESSAGES = [
     MEOW,
     TRADITIONAL_CAT,
     'meow',
     '瞄',
     '錨',
-    'ㄇㄧㄠ'
-] + POSSIBLE_MEOW_REACTS
+    'ㄇㄧㄠ',
+    
+] + POSSIBLE_MEOW_REACTS + JP_MEOWs
 
 CHISOBCAT = '<:ChisobCat:1157361078452375582>'
 SHIBELOL = '<:dogekek:1132350110148333718>'
 
 MEOW_REGEX = compile(
-    rf"\b(?:{'|'.join(POSSIBLE_MEOW_MESSAGES)})",
+    rf"(?:{'|'.join(POSSIBLE_MEOW_MESSAGES)})",
     flags=IGNORECASE | UNICODE,
 )
