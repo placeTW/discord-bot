3dae260eeb2f768e9d7b4072ec7531dc48772a84
chanomkaimuk <22185824+chanomkaimuk@users.noreply.github.com>
2024-07-13 01:09:29 +0200

    renamed presence/ to activities/; removed code from playing and watching; add more movies to watching_consts

---
+++ activities/watching_consts.py
@@ -0,0 +1,49 @@
+from discord.app_commands import Choice
+
+LIST_MOVIES_CMD_NAME = "list_movies"
+
+LIST_OF_MOVIES = (
+    "City of Sadness / 悲情城市",
+    "7 Days in Heaven / 父後七日",
+    "Detention / 返校",
+    "Cape No. 7 / 海角七號",
+    "The World Between Us / 我們與惡的距離",
+    "Spicy Teacher / 麻辣鮮師",
+    "Warriors Of The Rainbow / 賽德克·巴萊",
+    "The Teenage Psychic / 通靈少女",
+    "KANO (2014)",
+    "Incantation / 咒",
+    "You Are the Apple of My Eye / 那些年，我們一起追的女孩",
+    "Din Tao: Leader of the Parade / 陣頭",
+    "Light the Night / 華燈初上",
+    "Gold Leaf / 茶金",
+    "Alifu / 阿里夫/芙",
+    "Your Name Engraved Herein / 刻在我心底的名字",
+    "Our Times / 我的少女時代",
+    "The Dull-Ice Flower / 魯冰花",
+    "Zone Pro Site / 總舖師",
+    "Beyond Beauty: Taiwan from Above / 看見台灣",
+    "Monga / 艋舺",
+    "The Tag-Along / 紅衣小女孩",
+    "Till We Meet Again / 月老",
+    "Little Big Women / 孤味",
+    "Dear Tenant / 親愛的房客",
+    "Marry My Dead Body / 關於我和鬼變成家人的那件事",
+    "My Missing Valentine / 消失的情人節",
+    "Days / 日子",
+    "Classmates Minus / 同學麥娜絲",
+    "The Silent Forest / 無聲",
+    "I WeirDO / 怪胎",
+    "The Falls / 瀑布",
+    "Man in Love / 當男人戀愛時",
+    "Goddamned Asura / 該死的阿修羅",
+    "Gaga / 哈勇家",
+    "Incantation / 咒",
+    "The Post-Truth World / 罪後真相",
+    "The Pig, the Snake and the Pigeon / 周處除三害",
+    "Taiwan Crime Stories / 台灣犯罪故事",
+)
+
+# MOVIE_CHOICES = [
+#     Choice(name=movie_name, value=movie_name) for movie_name in LIST_OF_MOVIES
+# ]
