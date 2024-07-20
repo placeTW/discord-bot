4755c1ea9222ef3381a6c46f62bcabef87986387
chanomkaimuk <22185824+chanomkaimuk@users.noreply.github.com>
2024-07-20 15:26:52 +0200

    better skeleton (following tic tac toe example)

--- modules/quiz/multiple_choice.py
+++ modules/quiz/multiple_choice.py
@@ -1,20 +1,36 @@
 import discord
 
-def create_multiple_choice_embed(correct_choices: list, wrong_chocies: list) -> discord.Embed:
-    """
-    Create a multiple choice embed.
-    
-    Parameters:
-    - correct_choices: list of correct choices
-    - wrong_choices: list of wrong choices
-    """
-    embed = discord.Embed(
-        title="Multiple Choice",
-        description="Choose the correct answer",
-        color=discord.Color.green()
-    )
-    for i, choice in enumerate(correct_choices):
-        embed.add_field(name=f"Choice {i+1}", value=choice, inline=False)
-    for i, choice in enumerate(wrong_chocies):
-        embed.add_field(name=f"Choice {i+1+len(correct_choices)}", value=choice, inline=False)
-    return embed
+class QuizMultipleChoiceButton(discord.ui.Button["MultipleChoiceButton"]):
+    def __init__(self, x: int, y: int, label: str, is_correct: bool):
+        super().__init__(
+            style=discord.ButtonStyle.secondary, # need to change this?
+            label=label,
+            row=y
+        )
+        self.x = x
+        self.y = y
+        self.label = label
+        self.is_correct = is_correct
+
+    async def callback(self, interaction: discord.Interaction):
+        assert self.view is not None, "View not found"
+        view: MultipleChoiceButton = self.view
+        # if correct, change style to green and disable all buttons
+        if self.is_correct:
+            self.style = discord.ButtonStyle.success
+            for child in view.children:
+                child.disabled = True
+        # if incorrect, change style to red and disable this button
+        else:
+            self.style = discord.ButtonStyle.danger
+            self.disabled = True
+        await interaction.response.edit_message(view=view)
+
+class MultipleChoiceButton(discord.ui.View):
+    def __init__(self, choices: list[QuizChoice]):
+        super().__init__()
+
+class QuizChoice:
+    def __init__(self, label: str, is_correct: bool):
+        self.label = label
+        self.is_correct = is_correct
