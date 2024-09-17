ecd8e4e6794939ce8f5ef83f14fd64dae46c44e7
howardt12345 <howardt12345@gmail.com>
2024-09-17 10:03:20 -0400

    refactor: metro -> trains

--- commands/trains/trains.py
+++ commands/trains/trains.py
@@ -6,7 +6,7 @@ from random import choice
 from bot import TWPlaceClient
 import os
 import discord
-from .consts import METRO_CHOICES, METRO_DICT
+from .consts import TRAINS_CHOICES, TRAINS_DICT
 
 def register_commands(tree, client: TWPlaceClient, guilds: list[discord.Object]):
     @tree.command(
@@ -14,21 +14,21 @@ def register_commands(tree, client: TWPlaceClient, guilds: list[discord.Object])
         description="Get map of a train system",
         guilds=guilds,
     )
-    @app_commands.choices(city=METRO_CHOICES)
+    @app_commands.choices(city=TRAINS_CHOICES)
     @app_commands.describe(city="The train system to get the map of")
-    async def metro_cmd(
+    async def get_train_map(
         interaction: discord.Interaction,
-        city: Choice[str],
+        location: Choice[str],
     ):
-        """This function fetches the map of a metro station.
+        """This function fetches the train map of a given location.
 
         Args:
             interaction (discord.Interaction): required by discord.py
-            city (str): The city of the metro station to fetch.
+            location (str): The location of the train system to get the map of
         """
         # get the selected city
-        selected_city = city.value
+        selected_city = location.value
         # get the path to the url
-        img_url = METRO_DICT[selected_city]
+        img_url = TRAINS_DICT[selected_city]
         # send the url (not an image)
         await interaction.response.send_message(img_url)
