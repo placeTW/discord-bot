0e380eadf99916b5d0aa6b71a14553fb33537a0e
howardt12345 <howardt12345@gmail.com>
2024-09-18 22:41:06 -0400

    feat: Taiwan #1

--- tests/test_reacts/test_reacts_tw.py
+++ tests/test_reacts/test_reacts_tw.py
@@ -57,6 +57,53 @@ TEST_CASE_TW = (
     "美麗島",
 )
 
+TEST_CASES_TW_NO_1_TRUE = [
+    "Taiwan #1",
+    "Taiwan number 1",
+    "Taiwan is number one",
+    "Taiwan Number One",
+    "Taiwan is #1",
+    "Taiwan No. 1",
+    "Taiwan n° 1",
+    "Taiwán número uno",
+    "臺灣第一",
+    "台灣最棒",
+    "Taiwan is the best",
+    "Taiwan = first",
+    "Taiwan: top",
+    "Taiwan are greatest",
+    "Taiwan - number 1",
+    "Taiwan    #    1",
+    "臺灣第一名",
+    "台灣最強",
+    "台灣是第一",
+    "Taiwan 第一",
+    "臺灣 number one",
+    "Taiwan won",
+    "Taiwan ①",
+    "Taiwan 壹",
+    "TAIWAN NUMBER ONE",
+    "taiwan #1",
+    "I believe Taiwan is truly number one in Asia",
+    "Taiwan, often called Formosa, is number 1 in my heart"
+]
+
+TEST_CASES_TW_NO_1_FALSE = [
+    "Thailand #1",
+    "Taiwan is great",
+    "Number 1 Taiwan",
+    "Taiwan number 2",
+    "Taiwan was number 1",
+    "Taiwanese number 1",
+    "Taiwan numbers",
+    "Taiwan",
+    "#1",
+    "Number one",
+    "Taiwan #",
+    "Taiwan number"
+]
+
+
 ALL_TEST_CASES = TEST_CASE_EN + TEST_CASE_TW
 
 
@@ -100,3 +147,33 @@ def test_react_tw_regex_no_match(test_str: str):
     """Tests that these strings return FALSE."""
     # * surrounded by text
     assert not check_resource_match(f"a{test_str}b", resource_name='tw')
+
+@pytest.mark.parametrize(
+    "test_str",
+    TEST_CASES_TW_NO_1_TRUE,
+)
+def test_react_tw_no_1_regex_yes_match(test_str: str):
+    """Tests that these strings return TRUE."""
+    # * isolated string
+    assert check_resource_match(test_str, resource_name='tw')
+    # * surrounded by spaces
+    assert check_resource_match(f" {test_str} ", resource_name='tw')
+    # * to lowercase
+    assert check_resource_match(test_str.lower(), resource_name='tw')
+    # * to title case
+    assert check_resource_match(test_str.title(), resource_name='tw')
+
+@pytest.mark.parametrize(
+    "test_str",
+    TEST_CASES_TW_NO_1_FALSE,
+)
+def test_react_tw_no_1_regex_no_match(test_str: str):
+    """Tests that these strings return FALSE."""
+    # * isolated string
+    assert not check_resource_match(test_str, resource_name='tw')
+    # * surrounded by spaces
+    assert not check_resource_match(f" {test_str} ", resource_name='tw')
+    # * to lowercase
+    assert not check_resource_match(test_str.lower(), resource_name='tw')
+    # * to title case
+    assert not check_resource_match(test_str.title(), resource_name='tw')
