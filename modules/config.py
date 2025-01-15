24ff92b311c82a583186176dcd992ab62791b6ea
Howard Tseng <howardt12345@gmail.com>
2025-01-14 20:41:29 -0500

    code formatting

--- modules/config.py
+++ modules/config.py
@@ -10,34 +10,43 @@ def fetch_configs(is_prod: bool):
 
 
 def fetch_guild_config(guild_id: int, is_prod: bool):
-    response = supabaseClient.table("server_config").select("*").eq("guild_id", guild_id).eq(
-        "prod_config", is_prod
-    ).execute()
+    response = (
+        supabaseClient.table("server_config").select("*").eq("guild_id", guild_id).eq("prod_config", is_prod).execute()
+    )
     # if there is no data, return an empty dict
     if len(response.data) == 0:
         return {}
     return response.data[0]
 
+
 def create_new_config(guild_id: int, server_name: str, is_prod: bool):
-    response = supabaseClient.table("server_config").insert(
-        {
-            "guild_id": guild_id,
-            "server_name": server_name,
-            "prod_config": is_prod,
-        }
-    ).execute()
+    response = (
+        supabaseClient.table("server_config")
+        .insert(
+            {
+                "guild_id": guild_id,
+                "server_name": server_name,
+                "prod_config": is_prod,
+            }
+        )
+        .execute()
+    )
     return response.data
 
 
 def set_config(guild_id: int, key: str, value: str, is_prod: bool) -> list:
-    response = supabaseClient.table("server_config").update({key: value}).eq("guild_id", guild_id).eq(
-        "prod_config", is_prod
-    ).execute()
+    response = (
+        supabaseClient.table("server_config")
+        .update({key: value})
+        .eq("guild_id", guild_id)
+        .eq("prod_config", is_prod)
+        .execute()
+    )
     return response.data
+
 
 def remove_config(guild_id: int, is_prod: bool) -> list:
-    response = supabaseClient.table("server_config").delete().eq("guild_id", guild_id).eq(
-        "prod_config", is_prod
-    ).execute()
+    response = (
+        supabaseClient.table("server_config").delete().eq("guild_id", guild_id).eq("prod_config", is_prod).execute()
+    )
     return response.data
-
